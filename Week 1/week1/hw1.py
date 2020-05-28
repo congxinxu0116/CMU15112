291c960e3e16c7cb1095d3a3afe53803cfb0e1ac
congxinxu0116 <65978823+congxinxu0116@users.noreply.github.com>
2020-05-27 20:21:31 -0400

    Add homework files

---
+++ Week 1/week1/hw1.py
@@ -0,0 +1,248 @@
+#################################################
+# hw1.py
+# http://www.kosbie.net/cmu/fall-19/15-112/notes/hw1.html
+# Your name: David
+# Your andrew id:
+#################################################
+
+import cs112_f19_week1_linter
+import math
+
+#################################################
+# Helper functions
+#################################################
+
+def almostEqual(d1, d2, epsilon=10**-7):
+    # note: use math.isclose() outside 15-112 with Python version 3.5 or later
+    return (abs(d2 - d1) < epsilon)
+
+import decimal
+def roundHalfUp(d):
+    # Round to nearest with ties going away from zero.
+    rounding = decimal.ROUND_HALF_UP
+    # See other rounding options here:
+    # https://docs.python.org/3/library/decimal.html#rounding-modes
+    return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
+
+#################################################
+# Functions for you to write
+#################################################
+
+def nearestOdd(n):
+    if (n % 2 == 1):
+        return roundHalfUp(n)
+    elif (roundHalfUp(n / 2) >= n/2):
+        return roundHalfUp((n / 2)*2 - 1)
+    else:
+        return roundHalfUp((n / 2)*2 + 1)
+
+def isPerfectSquare(n):
+    if (type(n) == int and 
+        n >= 0 and 
+        math.sqrt(n) == roundHalfUp(math.sqrt(n))):
+        return True
+    return False
+
+def numberOfPoolBalls(rows):
+    if (rows == 0):
+        return 0
+    return (rows*(rows+1))/2
+
+def numberOfPoolBallRows(balls):
+    x = (math.sqrt(balls * 8 + 1) - 1) / 2
+    if (balls == 0):
+        return 0
+    elif (x % 1 == 0):
+        return x
+    else:
+        return x // 1 + 1
+
+def colorBlender(rgb1, rgb2, midpoints, n):
+    
+    if (n == 0): 
+        return rgb1
+
+    def mid(x, y, mp, n):
+        if (x < y):
+            return roundHalfUp(x + abs(x - y)/(mp + 1) * n)
+        else:
+            return roundHalfUp(x - abs(x - y)/(mp + 1) * n)
+    
+    red1 = rgb1 // 10**6
+    green1 = rgb1 // 10**3 - red1 * 12000**3
+    blue1 = rgb1 - red1 * 10**6 - green1 * 10 **3
+    # print(red1, green1, blue1)
+    
+    red2 = rgb2 // 10**6
+    green2 = rgb2 // 10**3 - red2 * 10**3
+    blue2 = rgb2 - red2 * 10**6 - green2 * 10 **3
+    # print(red2, green2, blue2)
+
+    if (n > 0 and n <= (midpoints + 1)):
+        red_mid = mid(x = red1, y = red2, mp = midpoints, n = n)
+        green_mid = mid(x = green1, y = green2, mp = midpoints, n = n)
+        blue_mid = mid(x = blue1, y = blue2, mp = midpoints, n = n)
+        return red_mid*(10**6) + green_mid*(10**3) + blue_mid
+
+#################################################
+# Bonus/Optional functions for you to write
+#################################################
+
+def bonusPlayThreeDiceYahtzee(dice):
+    return 42
+
+def bonusFindIntRootsOfCubic(a, b, c, d):
+    return 42
+
+#################################################
+# Test Functions
+#################################################
+
+def testNearestOdd():
+    print('Testing nearestOdd()... ', end='')
+    assert(nearestOdd(13) == 13)
+    assert(nearestOdd(12.001) == 13)
+    assert(nearestOdd(12) == 11)
+    assert(nearestOdd(11.999) == 11)
+    assert(nearestOdd(-13) == -13)
+    assert(nearestOdd(-12.001) == -13)
+    assert(nearestOdd(-12) == -13)
+    assert(nearestOdd(-11.999) == -11)
+    # results must be int's not floats
+    assert(isinstance(nearestOdd(13.0), int))
+    assert(isinstance(nearestOdd(11.999), int))
+    print('Passed.')
+
+def testIsPerfectSquare():
+    print('Testing isPerfectSquare()... ', end='')
+    assert(isPerfectSquare(0) == True)
+    assert(isPerfectSquare(1) == True)
+    assert(isPerfectSquare(16) == True)
+    assert(isPerfectSquare(1234**2) == True)
+    assert(isPerfectSquare(15) == False)
+    assert(isPerfectSquare(17) == False)
+    assert(isPerfectSquare(-16) == False)
+    assert(isPerfectSquare(1234**2+1) == False)
+    assert(isPerfectSquare(1234**2-1) == False)
+    assert(isPerfectSquare(4.0000001) == False)
+    assert(isPerfectSquare('Do not crash here!') == False)
+    print('Passed.')
+
+def testNumberOfPoolBalls():
+    print('Testing numberOfPoolBalls()... ', end='')
+    assert(numberOfPoolBalls(0) == 0)
+    assert(numberOfPoolBalls(1) == 1)
+    assert(numberOfPoolBalls(2) == 3)   # 1+2 == 3
+    assert(numberOfPoolBalls(3) == 6)   # 1+2+3 == 6
+    assert(numberOfPoolBalls(10) == 55) # 1+2+...+10 == 55
+    print('Passed.')
+
+def testNumberOfPoolBallRows():
+    print('Testing numberOfPoolBallRows()... ', end='')
+    assert(numberOfPoolBallRows(0) == 0)
+    assert(numberOfPoolBallRows(1) == 1)
+    assert(numberOfPoolBallRows(2) == 2)
+    assert(numberOfPoolBallRows(3) == 2)
+    assert(numberOfPoolBallRows(4) == 3)
+    assert(numberOfPoolBallRows(6) == 3)
+    assert(numberOfPoolBallRows(7) == 4)
+    assert(numberOfPoolBallRows(10) == 4)
+    assert(numberOfPoolBallRows(11) == 5)
+    assert(numberOfPoolBallRows(55) == 10)
+    assert(numberOfPoolBallRows(56) == 11)
+    print('Passed.')
+
+def testColorBlender():
+    print('Testing colorBlender()... ', end='')
+    # http://meyerweb.com/eric/tools/color-blend/#DC143C:BDFCC9:3:rgbd
+    assert(colorBlender(220020060, 189252201, 3, -1) == None)
+    assert(colorBlender(220020060, 189252201, 3, 0) == 220020060)
+    assert(colorBlender(220020060, 189252201, 3, 1) == 212078095)
+    assert(colorBlender(220020060, 189252201, 3, 2) == 205136131)
+    assert(colorBlender(220020060, 189252201, 3, 3) == 197194166)
+    assert(colorBlender(220020060, 189252201, 3, 4) == 189252201)
+    assert(colorBlender(220020060, 189252201, 3, 5) == None)
+    # http://meyerweb.com/eric/tools/color-blend/#0100FF:FF0280:2:rgbd
+    assert(colorBlender(1000255, 255002128, 2, -1) == None)
+    assert(colorBlender(1000255, 255002128, 2, 0) == 1000255)
+    assert(colorBlender(1000255, 255002128, 2, 1) == 86001213)
+    assert(colorBlender(1000255, 255002128, 2, 2) == 170001170)
+    assert(colorBlender(1000255, 255002128, 2, 3) == 255002128)
+    print('Passed.')
+
+def testBonusPlayThreeDiceYahtzee():
+    print('Testing bonusPlayThreeDiceYahtzee()...', end='')
+    assert(handToDice(123) == (1,2,3))
+    assert(handToDice(214) == (2,1,4))
+    assert(handToDice(422) == (4,2,2))
+    assert(diceToOrderedHand(1,2,3) == 321)
+    assert(diceToOrderedHand(6,5,4) == 654)
+    assert(diceToOrderedHand(1,4,2) == 421)
+    assert(diceToOrderedHand(6,5,6) == 665)
+    assert(diceToOrderedHand(2,2,2) == 222)
+    assert(playStep2(413, 2312) == (421, 23))
+    assert(playStep2(421, 23) == (432, 0))
+    assert(playStep2(413, 2345) == (544, 23))
+    assert(playStep2(544, 23) == (443, 2))
+    assert(playStep2(544, 456) == (644, 45))
+    assert(score(432) == 4)
+    assert(score(532) == 5)
+    assert(score(443) == 10+4+4)
+    assert(score(633) == 10+3+3)
+    assert(score(333) == 20+3+3+3)
+    assert(score(555) == 20+5+5+5)
+    assert(bonusPlayThreeDiceYahtzee(2312413) == (432, 4))
+    assert(bonusPlayThreeDiceYahtzee(2315413) == (532, 5))
+    assert(bonusPlayThreeDiceYahtzee(2345413) == (443, 18))
+    assert(bonusPlayThreeDiceYahtzee(2633413) == (633, 16))
+    assert(bonusPlayThreeDiceYahtzee(2333413) == (333, 29))
+    assert(bonusPlayThreeDiceYahtzee(2333555) == (555, 35))
+    print('Passed!')
+
+def getCubicCoeffs(k, root1, root2, root3):
+    # Given roots e,f,g and vertical scale k, we can find
+    # the coefficients a,b,c,d as such:
+    # k(x-e)(x-f)(x-g) =
+    # k(x-e)(x^2 - (f+g)x + fg)
+    # kx^3 - k(e+f+g)x^2 + k(ef+fg+eg)x - kefg
+    e,f,g = root1, root2, root3
+    return k, -k*(e+f+g), k*(e*f+f*g+e*g), -k*e*f*g
+
+def testFindIntRootsOfCubicCase(k, z1, z2, z3):
+    a,b,c,d = getCubicCoeffs(k, z1, z2, z3)
+    result1, result2, result3 = bonusFindIntRootsOfCubic(a,b,c,d)
+    m1 = min(z1, z2, z3)
+    m3 = max(z1, z2, z3)
+    m2 = (z1+z2+z3)-(m1+m3)
+    actual = (m1, m2, m3)
+    assert(almostEqual(m1, result1))
+    assert(almostEqual(m2, result2))
+    assert(almostEqual(m3, result3))
+
+def testBonusFindIntRootsOfCubic():
+    print('Testing bonusFindIntRootsOfCubic()...', end='')
+    testFindIntRootsOfCubicCase(5, 1, 3,  2)
+    testFindIntRootsOfCubicCase(2, 5, 33, 7)
+    testFindIntRootsOfCubicCase(-18, 24, 3, -8)
+    testFindIntRootsOfCubicCase(1, 2, 3, 4)
+    print('Passed.')
+
+#################################################
+# testAll and main
+#################################################
+
+def testAll():
+    testNearestOdd()
+    testIsPerfectSquare()
+    testNumberOfPoolBalls()
+    testNumberOfPoolBallRows()
+    testColorBlender()
+    # testBonusPlayThreeDiceYahtzee()
+    # testBonusFindIntRootsOfCubic()
+
+def main():
+    cs112_f19_week1_linter.lint()
+    testAll()
+
+if __name__ == '__main__':
+    main()
