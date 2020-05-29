355c1f1430ec6764bb912af372447b865c919566
congxinxu0116 <65978823+congxinxu0116@users.noreply.github.com>
2020-05-29 11:45:42 -0400

    Update GraphicsPart1.py

--- Week 2/GraphicsPart1.py
+++ Week 2/GraphicsPart1.py
@@ -1,2 +1,38 @@
 # Graphics in Tkinter Part 1
 # Create an Empty Canvas
+
+from tkinter import *
+
+def draw(canvas, width, height):
+    # create_line (x1, y1, x2, y2) draws a line 
+    #   from (x1, y1) to (x2, y2)
+    canvas.create_line(25, 50, width/2, height/2)
+
+def runDrawing(width=300, height=300):
+    root = Tk()
+    root.resizable(width=False, height=False) # prevents resizing window
+    canvas = Canvas(root, width=width, height=height)
+    canvas.configure(bd=0, highlightthickness=0)
+    canvas.pack()
+    draw(canvas, width, height)
+    root.mainloop()
+    print("bye!")
+
+runDrawing(400, 200)
+
+from tkinter import *
+
+def draw(canvas, width, height):
+    pass # replace with your drawing code!
+
+def runDrawing(width=300, height=300):
+    root = Tk()
+    root.resizable(width=False, height=False) # prevents resizing window
+    canvas = Canvas(root, width=width, height=height)
+    canvas.configure(bd=0, highlightthickness=0)
+    canvas.pack()
+    draw(canvas, width, height)
+    root.mainloop()
+    print("bye!")
+
+runDrawing(400, 200)
