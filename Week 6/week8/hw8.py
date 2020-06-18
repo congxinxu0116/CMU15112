bd03e8e8a2725ead7f1442dab7ec8576bad429e5
congxinxu0116 <65978823+congxinxu0116@users.noreply.github.com>
2020-06-18 09:44:36 -0400

    Update hw8.py

--- Week 6/week8/hw8.py
+++ Week 6/week8/hw8.py
@@ -28,18 +28,95 @@ def roundHalfUp(d):
 # Functions for you to write
 #################################################
 
+def getPairSum(lst, target):
+    if (len(lst) > 1):
+        s = set()
+        for i in lst:
+            if (target - i in s):
+                return (i, target - i)
+            else:
+                s.add(i)
+
+def containsPythagoreanTriple(lst):
+    lst = [i**2 for i in lst]
+    # print(lst)
+    if (len(lst) > 2):
+        s = set()
+        i = 0
+        while (i < len(lst)):
+            j = i + 1
+            while (j < len(lst)):
+                if (lst[i] + lst[j] in s 
+                    or abs(lst[i] - lst[j]) in s):                    
+                    return True
+                else:
+                    s.add(lst[i])
+                    s.add(lst[j])
+                # print(i, j)
+                j += 1
+            i += 1
+    return False
+
+def movieAwards(oscarResults):
+    d = dict()
+    for i in oscarResults:
+        tmp = 1 + d.get(i[1], 0)
+        d[i[1]] = tmp
+    return d
+
 #################################################
 # Test Functions
 #################################################
 
-# ADD YOUR OWN TEST FUNCTIONS!!!
+def testgetPairSum():
+    print('Testing testgetPairSum()... ', end='')
+    assert(getPairSum([1], 1) == None)
+    assert(getPairSum([5, 2], 7) in [ (5, 2), (2, 5) ])
+    assert(getPairSum([10, -1, 1, -8, 3, 1], 2) in
+             [ (10, -8), (-8, 10), (-1, 3), (3, -1), (1, 1) ])
+    assert(getPairSum([10, -1, 1, -8, 3, 1], 10) == None)
+    print('Passed.')
 
+def testcontainsPythagoreanTriple():
+    print('Testing containsPythagoreanTriple()... ', end='')
+    assert(containsPythagoreanTriple([1, 3, 6, 2, 5, 1, 4]) == True)
+    assert(containsPythagoreanTriple([1, 3, 6, 2, 1, 4]) == False)
+    assert(containsPythagoreanTriple([5,1,8,2,12,13,4]) == True)
+    
+    print('Passed.')
+
+def testmovieAwards():
+    print('Testing movieAwards()... ', end='')
+    a = { 
+        ("Best Picture", "Green Book"), 
+        ("Best Actor", "Bohemian Rhapsody"),
+        ("Best Actress", "The Favourite"),
+        ("Film Editing", "Bohemian Rhapsody"),
+        ("Best Original Score", "Black Panther"),
+        ("Costume Design", "Black Panther"),
+        ("Sound Editing", "Bohemian Rhapsody"),
+        ("Best Director", "Roma")
+    }   
+
+    b = { 
+        "Black Panther" : 2,
+        "Bohemian Rhapsody" : 3,
+        "The Favourite" : 1,
+        "Green Book" : 1,
+        "Roma" : 1
+    }
+    assert(movieAwards(a) == b)
+    
+    print('Passed.')
 #################################################
 # testAll and main
 #################################################
 
 def testAll():
-    pass
+    testgetPairSum()
+    testcontainsPythagoreanTriple()
+    testmovieAwards()
+    print("All Good")
 
 def main():
     cs112_f19_week8_linter.lint()
